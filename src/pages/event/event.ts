68357abf97fd54a8cb0764d15f8dc7b648ab8917
Roussange <roussange.alexandre@gmail.com>
2017-03-27 17:12:55 +0200

    remove useless import

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -3,7 +3,6 @@ import {Component} from '@angular/core';
 import {Events, NavController} from 'ionic-angular';
 import {EventDescriptionPage} from "./event-description/event-description";
 import {GlobalEvents} from "../../app/providers/events";
-import {UserData} from "../../app/providers/user";
 
 @Component({
   selector: 'page-event',
